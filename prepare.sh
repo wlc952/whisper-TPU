e62097586d46009ad5d19c52d20a4edf7d4ce5c1
linaro <wlc@zju.edu.cn>
2024-10-29 14:04:50 +0800

    update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,8 @@
 sudo apt install unzip
 pip3 install --upgrade pip
 pip3 install --no-cache-dir -r python/requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu
-pip3 install python_wheels/sophon_arm-3.8.0-py3-none-any.whl
+
+if ! python3 -c "import sophon.sail" &> /dev/null; then
+    echo "Sophon SAIL is not found, installing..."
+    pip3 install https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/sophon_arm-3.8.0-py3-none-any.whl
+fi
